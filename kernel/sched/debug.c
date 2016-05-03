f42577ab3d3c43c3f2fe299bbb474d44c89869ad
Wanpeng Li <wanpeng.li@hotmail.com>
2016-05-03 12:38:25 +0800

    sched/debug: Print out idle balance values even on !CONFIG_SCHEDSTATS kernels
    The max_idle_balance_cost and avg_idle values which are tracked and ar used to
    capture short idle incidents, are not associated with schedstats, however the
    information of these two values isn't printed out on !CONFIG_SCHEDSTATS kernels.
    
    Fix this by moving the value printout out of the CONFIG_SCHEDSTATS section.
    
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1462250305-4523-1-git-send-email-wanpeng.li@hotmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -691,14 +691,15 @@ do {									\
 #undef P
 #undef PN
 
-#ifdef CONFIG_SCHEDSTATS
-#define P(n) SEQ_printf(m, "  .%-30s: %d\n", #n, rq->n);
-#define P64(n) SEQ_printf(m, "  .%-30s: %Ld\n", #n, rq->n);
-
 #ifdef CONFIG_SMP
+#define P64(n) SEQ_printf(m, "  .%-30s: %Ld\n", #n, rq->n);
 	P64(avg_idle);
 	P64(max_idle_balance_cost);
+#undef P64
 #endif
+
+#ifdef CONFIG_SCHEDSTATS
+#define P(n) SEQ_printf(m, "  .%-30s: %d\n", #n, rq->n);
 	if (schedstat_enabled()) {
 		P(yld_count);
 		P(sched_count);
@@ -707,7 +708,6 @@ do {									\
 		P(ttwu_local);
 	}
 #undef P
-#undef P64
 #endif
 	spin_lock_irqsave(&sched_debug_lock, flags);
 	print_cfs_stats(m, cpu);
