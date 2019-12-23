2ff99a3bb96c30e3e7c53161c4e999453e5f9f3b
Qiwu Huang <huangqiwu@xiaomi.com>
2019-12-23 11:24:53 +0800

    drivers: media: camera_v2: Fix camera in new miui version
    Change-Id: Ieaf48540e86528b29cfed8e4a2fb67200a761c70
    Signed-off-by: Qiwu Huang <huangqiwu@xiaomi.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -811,6 +811,7 @@ int32_t msm_sensor_driver_probe(void *setting,
 		slave_info->sensor_id_info = slave_info32->sensor_id_info;
 		slave_info->vendor_id_info = slave_info32->vendor_id_info;
 		slave_info->vcm_id_info = slave_info32->vcm_id_info;
+		slave_info->lens_id_info = slave_info32->lens_id_info;
 
 		slave_info->slave_addr = slave_info32->slave_addr;
 		slave_info->power_setting_array.size =
@@ -1044,8 +1045,9 @@ CSID_TG:
 	s_ctrl->sensordata->flash_name = slave_info->flash_name;
 	s_ctrl->sensordata->vendor_id_info = &(slave_info->vendor_id_info);
 	s_ctrl->sensordata->vcm_id_info = &(slave_info->vcm_id_info);
+	s_ctrl->sensordata->lens_id_info = &(slave_info->lens_id_info);
 	/*
-	 * Update eeporm subdevice Id by input eeprom name
+	 * Update eeprom subdevice Id by input eeprom name
 	 */
 	rc = msm_sensor_fill_eeprom_subdevid_by_name(s_ctrl);
 	if (rc < 0) {
