6054321374845d92eb78a8342df2e65b75a442a2
Guru Das Srinagesh <gurus@codeaurora.org>
2020-11-08 22:49:44 +0800

    power: power_supply: Add property for capping parallel FCC
    Add a power supply property to expose the upper limit on the charging
    current that the parallel charger can supply.
    
    This is different from POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX in
    that the latter is meant for querying the hardware-defined value, while
    this new property is meant to be used to restrict the parallel's share
    of FCC to its specified limit much like in SMB1354, for instance.
    
    CRs-Fixed: 2170280
    Change-Id: I3a6ed44fbafe0eff4ecc9462bdf9a35f34811d8c
    Signed-off-by: Guru Das Srinagesh <gurus@codeaurora.org>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -276,6 +276,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_RERUN_APSD,
 	POWER_SUPPLY_PROP_PARALLEL_BATFET_MODE,
 	POWER_SUPPLY_PROP_MIN_ICL,
+	POWER_SUPPLY_PROP_PARALLEL_FCC_MAX,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
