6fc6398eb7ab34b50be175a8a4911370f3e015ad
Rick Yiu <rickyiu@google.com>
2022-02-11 22:10:32 +0800

    sched/fair: schedule lower priority tasks from little cores
    With scheduler placement hint, there still could be several boosted
    tasks contending for big cores. On chipset with fewer big cores, it
    might cause problems like jank. To improve it, schedule tasks of prio
    >= DEFAULT_PRIO from little cores if they could fit, even for tasks
    that prefer high capacity cpus, since such prio means they are less
    important.
    
    Bug: 158936596
    Test: tasks scheduled as expected
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: Ic0cc06461818944e3e97ec0493c0d9c9f1a5e217
    
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3918,11 +3918,13 @@ static inline bool task_fits_capacity(struct task_struct *p,
 	 * CPU.
 	 */
 	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
-		margin = schedtune_task_boost(p) > 0 ?
+		margin = schedtune_task_boost(p) > 0 &&
+			   p->prio <= DEFAULT_PRIO ?
 			sched_capacity_margin_down_boosted[task_cpu(p)] :
 			sched_capacity_margin_down[task_cpu(p)];
 	else
-		margin = schedtune_task_boost(p) > 0 ?
+		margin = schedtune_task_boost(p) > 0 &&
+			   p->prio <= DEFAULT_PRIO ?
 			sched_capacity_margin_up_boosted[task_cpu(p)] :
 			sched_capacity_margin_up[task_cpu(p)];
 
@@ -7180,7 +7182,7 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 		 */
 		if ((prefer_idle && best_idle_cpu != -1) ||
 		    (boosted && (best_idle_cpu != -1 || target_cpu != -1))) {
-			if (boosted) {
+			if (boosted && p->prio <= DEFAULT_PRIO) {
 				/*
 				 * For boosted task, stop searching when an idle
 				 * cpu is found in mid cluster.
