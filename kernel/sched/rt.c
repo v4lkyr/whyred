8ac6f539b6fde964e94212dec45d331a5753b878
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2022-02-12 10:30:03 +0800

    sched: rt: Fix double accounting of cpu util in task placement decision
    In RT energy aware task placement, cpu utilization is double accounted
    to check whether cpu would be overutilized for given task or not.
    
    Fix the double accounting the cpu util.
    
    Change-Id: Id51dd6c952126b870942746c0408fa387ec3bfb1
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1824,9 +1824,7 @@ retry:
 			if (cpu_isolated(cpu))
 				continue;
 
-			util = cpu_util(cpu);
-
-			if (__cpu_overutilized(cpu, util + tutil))
+			if (__cpu_overutilized(cpu, tutil))
 				continue;
 
 			util = cpu_util(cpu);
