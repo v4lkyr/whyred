Atom-X-Devs/android_kernel_xiaomi_sdm660

drivers: power: expose property rerun apsd status
* fixed logspam when charging or usb connected:
[    3.175620] get prop 201 is not supported in usb

Change-Id: I3bd243667029795cd148191867c80b9a6b403b48
Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

drivers/power/supply/qcom/qpnp-smb2.c
@@ -634,6 +634,11 @@ static int smb2_usb_get_prop(struct power_supply *psy,
 		val->intval = get_client_vote(chg->disable_power_role_switch,
 					      MOISTURE_VOTER);
 		break;
+#ifdef CONFIG_MACH_LONGCHEER
+	case POWER_SUPPLY_PROP_RERUN_APSD:
+		val->intval = chg->float_rerun_apsd;
+		break;
+#endif
 	default:
 		pr_err("get prop %d is not supported in usb\n", psp);
 		rc = -EINVAL;

v4lkyr/whyred

Signed-off-by: v4lkyr <valkyr23@gmail.com>

drivers/power/supply/qcom/qpnp-smb2.c
@@ -590,6 +590,11 @@ static int smb2_usb_get_prop(struct power_supply *psy,
 		val->intval = get_client_vote(chg->disable_power_role_switch,
 					      MOISTURE_VOTER);
 		break;
+#ifdef CONFIG_XIAOMI_WHYRED
+	case POWER_SUPPLY_PROP_RERUN_APSD:
+		val->intval = chg->float_rerun_apsd;
+		break;
+#endif
 	default:
 		pr_err("get prop %d is not supported in usb\n", psp);
 		rc = -EINVAL;